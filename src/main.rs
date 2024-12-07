177f517cc2e5245b54aa26209980f34a2e036101
Shane Sopel <shanesopel@gmail.com>
2024-12-06 20:42:19 -0500

    testing serial adc

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,7 @@ use cortex_m_rt::entry;
 //ensure we halt the program on panic.
 use defmt_rtt as _;
 
+use core::fmt::Write;
 // Alias for our HAL crate
 use rp2040_hal as hal;
 
@@ -30,11 +31,14 @@ use embedded_graphics::prelude::*;
 use embedded_graphics::pixelcolor::{Rgb565};
 //use embedded_time::rate::*;
 use embedded_hal::digital::v2::OutputPin;
+use embedded_hal::adc::OneShot;
 use fugit::RateExtU32;
 use rp2040_hal::clocks::Clock;
 //use rp2040_hal::entry;
 use st7735_lcd;
 use st7735_lcd::Orientation;
+// UART related types
+use hal::uart::{DataBits, StopBits, UartConfig};
 
 // A shorter alias for the Peripheral Access Crate, which provides low level
 // register access
@@ -66,7 +70,7 @@ fn main() -> !
     
     //grab our single ton objects.
     let mut pac = pac::Peripherals::take().unwrap();
-    //let core = pac::CorePeripherals::take().unwrap();
+    let core = pac::CorePeripherals::take().unwrap();
 
     // set up the watchdog driver - needed by the clock setup code
     let mut watchdog = hal::Watchdog::new(pac.WATCHDOG);
@@ -87,7 +91,7 @@ fn main() -> !
     //configure the system timer as a delay provider
     //Delay new takes in two parameters the system timer , and the ahb freq as a u32 integer.
     // the ahb bus is widely used in ARM7 and ARM9 cortex M based designs
-    //let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().integer());
+    let mut delay = cortex_m::delay::Delay::new(core.SYST, clocks.system_clock.freq().to_Hz());
     let mut timer = rp2040_hal::Timer::new(pac.TIMER, &mut pac.RESETS, &clocks);
 
 
@@ -123,6 +127,29 @@ fn main() -> !
         &embedded_hal::spi::MODE_0,
     );
 
+    let uart_pins =
+    (
+       pins.gpio0.into_function::<hal::gpio::FunctionUart>(),
+       pins.gpio1.into_function::<hal::gpio::FunctionUart>(),
+    );
+
+        // Create a UART driver
+        let mut uart = hal::uart::UartPeripheral::new(pac.UART0, uart_pins, &mut pac.RESETS)
+        .enable(
+            UartConfig::new(115200.Hz(), DataBits::Eight, None, StopBits::One),
+            clocks.peripheral_clock.freq(),
+        )
+        .unwrap();
+
+    // Write to the UART
+    uart.write_full_blocking(b"ADC example\r\n");
+
+    // Enable ADC
+    let mut adc = hal::Adc::new(pac.ADC, &mut pac.RESETS);
+
+    // Configure GPIO26 as an ADC input
+    let mut adc_pin_0 = hal::adc::AdcPin::new(pins.gpio26).unwrap();
+
     //setup the ST7735 display
     let mut disp = st7735_lcd::ST7735::new(spi, dc, rst, true, false, 160, 128);
 
@@ -137,36 +164,39 @@ fn main() -> !
         .draw(&mut disp)
         .unwrap();
 
-
     lcd_led.set_high().unwrap();
 
+    // Draw a filled square
+    let mut paddle1 =  Rectangle::with_corners(Point::new(2, 50+ 50), Point::new(2+16, 100+60))
+    .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1))
+    .draw(&mut disp)
+    .unwrap();
+
+    let mut paddle2 =  Rectangle::with_corners(Point::new(142, 50+ 50 ), Point::new(142 + 16, 100+60))
+      .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1))
+      .draw(&mut disp)
+      .unwrap();
     
+    loop {
+      // Read the raw ADC counts from the temperature sensor channel.
+      let pin_adc_counts: u16 = adc.read(&mut adc_pin_0).unwrap();
+      if pin_adc_counts == 4095
+      {
+        writeln!(
+          uart,
+          "ADC readings: is 4095"
+      )
+      .unwrap();
+      }
+      else 
+      {
+        writeln!(
+          uart,
+          "no input yet \n"    
+      )
+      .unwrap();
+      }
+      delay.delay_ms(1000);     
+    }
 
-    loop {  
-      let n = 1;
-      while n < 101
-        {
-          // Draw a filled square
-        let mut paddle1 =  Rectangle::with_corners(Point::new(2, 50+n), Point::new(2+16, 100+n))
-          .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1))
-          .draw(&mut disp)
-          .unwrap();
-      
-        let mut paddle2 =  Rectangle::with_corners(Point::new(142, 50+ n ), Point::new(142 + 16, 100+n))
-          .into_styled(PrimitiveStyle::with_stroke(Rgb565::GREEN, 1))
-          .draw(&mut disp)
-          .unwrap();
-
-         if (n != n)
-         {
-            paddle1;
-            paddle2;
-         }
-         
-          disp.clear(Rgb565::BLACK).unwrap();
-        } 
-      } 
-      
 }
-
-  
